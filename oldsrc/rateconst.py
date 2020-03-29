7e06a7ee82a5b39094ef01eb283786efd4be5aa0
Kenneth Geisshirt <geisshirt@gmail.com>
2020-03-29 11:39:36 +0200

    Bring back mdreac (#2)
    * Use GNU Fortran v8 to compile old source code
    * Allow larger integers in input file
    * Remove lost software from Makefile
    * Add new scripts to analyze data (rate constants and diffusion coefficients)

---
+++ oldsrc/rateconst.py
@@ -0,0 +1,35 @@
+# Calculate rate constants from event data
+# Processing of raw simulation data:
+# cat mdreac.reac | tr -s " " | cut -c2- | tr " " ","  | cut -f2- -d"," > mdreac.reac.csv
+
+import sys, getopt
+
+import numpy as np
+from numpy import genfromtxt
+
+def usage(prog):
+    print('Usage: ', prog, ' -d number -N number')
+    sys.exit(2)
+
+def main(argv):
+    rho = 0.8
+    N = 1024
+
+    try:
+        opts, args = getopt.getopt(argv, 'hd:N:')
+    except getopt.GetoptError:
+        usage(argv[0])
+    for opt, arg in opts:
+        if opt == '-h':
+            usage(argv[0])
+        elif opt == '-d':
+            rho = float(arg)
+        elif opt == '-N':
+            N = float(arg)
+
+    data = genfromtxt('mdreac.reac.csv', delimiter=',')
+    for m in np.mean(data, 0):
+        print(2*m/(rho*N))
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
